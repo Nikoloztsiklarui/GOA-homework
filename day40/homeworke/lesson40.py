57eb7045429107a0903426cd316a340e2a793404
Nikoloztsiklarui <nikoloz.tsiklauri11@gmail.com>
2024-07-14 16:49:17 +0400

    Goa homework
    commit

---
+++ day40/homeworke/lesson40.py
@@ -0,0 +1,10 @@
+# მომხმარებეკს უნდა შევეკითხოთ 3 სხვადასხვა სახელი და ეს სახელები უნდა დავამატოთ სიაში შემდეგ კი უნდა დავბეჭდოთ სიაში
+
+name = input("enter your name")
+name1 = input("enter your name")
+name2 = input("enter your name")
+listn = []
+listn.append(name  )
+listn.append(name1  )
+listn.append(name2  )
+print(listn)
